aef498739b5f27ae710c935c0c8c5e7eb9a266b6
maxmodapp <maximodapp@gmail.com>
2021-07-28 18:27:41 -0300

    sape

--- pasantias1/clsbotiquin.php
+++ pasantias1/clsbotiquin.php
@@ -1,7 +1,8 @@
 <?php
+// conexion con la base
 include_once 'php/conexion.php';
 
-
+// llamado a la tabla
 $sql_leer='SELECT * FROM clsbotiquin';
 $gsent=$pdo->prepare($sql_leer);
 $gsent->execute();
@@ -12,20 +13,25 @@ $usuarios=$gsent->fetchAll();
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css"/> <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous"> 
+    <!-- CSS -->
+    <link rel="text/css"href="style.css">
+    <link rel="stylesheet"href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet"href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <a href="agregar.php" ><img src="https://img.icons8.com/ios/50/000000/add--v1.png"></a>
   </head>
   <body>
-    <div class="container">
+      <div class="container">
        <div class="row">
            <div class="col-lg-12">
-            <table id="tablaUsuarios" class="hover row-border">
-                <thead class="text-center">
-                <th>Codigo</th>
+           <div class="table-responsive">
+                <table id="usuarios" class="table table-striped table-bordered" style="width:100%">
+                     <thead>
+                     <th>Codigo</th>
                 <th>Medicamento</th>
                 <th>Stock inicial</th>
-                </thead>
+                     </thead>
                 <tbody>
                     <?php
                         foreach($usuarios as $usuario){
@@ -34,7 +40,10 @@ $usuarios=$gsent->fetchAll();
                     <td><?php echo $usuario['Codigo']?></td>
                     <td><?php echo $usuario['Medicamento']?></td>
                     <td><?php echo $usuario['Stock inicial']?></td>
-                    </tr>
+                        <a href="editar.php?id=<?php echo $usuario['id']?>" class="float-right"> <i class="fas fa-pencil-alt"></i></a>
+                        <a href="eliminar.php?id=<?php echo $usuario['id']?>" class="float-right ml-3">
+                      <i class="fas fa-trash-alt"></i></a>
+                  </tr>
                     <?php
                         }
                     ?>
@@ -43,16 +52,26 @@ $usuarios=$gsent->fetchAll();
            </div>
        </div> 
     </div>
+   
+    
+
+    <!--JavaScript -->
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>  
-    <script src="popper/popper.min.js"> </script>
-    <script src="JavaScript/main.js"></script>                
-    <script src="datatables.min.js"></script>
-    <script src="Buttons-1.7.0/js/dataTables.buttons.min.js"></script>
-    <script src="JSZip-2.5.0/jszip.min.js"></script>    
-    <script src="pdfmake-0.1.36/pdfmake.min.js"></script>   
-    <script src="pdfmake-0.1.36/vfs_fonts.js"></script>
-    <script src="Buttons-1.7.0/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap.min.js"></script>   
+    <!-- links js -->
+    <script src="javascripts/js.js"></script>
+    <!-- datatables -->
+    <script src="Datatables/datatables.min.js"></script>
+    <!-- Botones -->
+    <script src="Datatables/Butons-1.7.0/js/dataTables.buttons.min.js"></script>
+    <script src="Datatables/JSZip-2.5.0/jszip.min.js"></script>
+    <script src="Datatables/pdfmake-0.1.36/pdfmake.min.js"></script>
+    <script src="Datatables/pdfmake-0.1.36/vfs_fonts.js"></script>
+    <script src="Datatables/Butons-1.7.0/js/buttons.html5.min.js"></script>
+    
+
+    
+    
   </body>
 </html>
