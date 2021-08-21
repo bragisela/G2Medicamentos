2abeacf6825821312605553746cdda68310b539b
facu19-r <facur19@hotmail.com>
2021-08-21 20:33:23 -0300

    Update BDD Relaciones.sql

--- BDD Relaciones.sql
+++ BDD Relaciones.sql
@@ -1,31 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 22-08-2021 a las 00:54:50
--- Versión del servidor: 10.4.13-MariaDB
--- Versión de PHP: 7.4.7
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `pasantias`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `clearing`
---
+DROP DATABASE IF EXISTS pasantias;
+CREATE DATABASE IF NOT EXISTS pasantias;
+USE pasantias;
 
 CREATE TABLE `clearing` (
   `Idclearing` int(5) NOT NULL,
@@ -35,7 +10,7 @@ CREATE TABLE `clearing` (
   `Cantidad` int(4) NOT NULL,
   `Admin` int(1) NOT NULL,
   `Idusuario` int(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+);
 
 --
 -- Volcado de datos para la tabla `clearing`
@@ -57,7 +32,7 @@ CREATE TABLE `clsbotiquin` (
   `Medicamento` char(50) NOT NULL,
   `Stock_inicial` int(5) NOT NULL,
   `Idusuario` int(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+);
 
 --
 -- Volcado de datos para la tabla `clsbotiquin`
@@ -89,7 +64,7 @@ CREATE TABLE `recetas` (
   `2Cod_medico` varchar(15) NOT NULL,
   `Cantidad2` int(5) NOT NULL,
   `Idusuario` int(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+);
 
 --
 -- Volcado de datos para la tabla `recetas`
@@ -108,7 +83,7 @@ INSERT INTO `recetas` (`Idrecetas`, `Fecha`, `Apellido`, `Nombres`, `Tipo_DNI`, 
 CREATE TABLE `roles` (
   `Idrol` int(2) NOT NULL,
   `Nombre` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+);
 
 --
 -- Volcado de datos para la tabla `roles`
@@ -131,7 +106,7 @@ CREATE TABLE `salidas` (
   `Cantidad` int(5) NOT NULL,
   `Motivo` char(255) NOT NULL,
   `Idusuario` int(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+);
 
 --
 -- Volcado de datos para la tabla `salidas`
@@ -153,7 +128,7 @@ CREATE TABLE `stockinicial` (
   `Medicamento` char(50) NOT NULL,
   `Stock_inicial` int(5) NOT NULL,
   `Idusuario` int(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+);
 
 --
 -- Volcado de datos para la tabla `stockinicial`
@@ -175,7 +150,7 @@ CREATE TABLE `usuarios` (
   `Apellido` text NOT NULL,
   `Idrol` int(2) NOT NULL,
   `Clave` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+);
 
 --
 -- Volcado de datos para la tabla `usuarios`
@@ -324,7 +299,3 @@ ALTER TABLE `stockinicial`
 ALTER TABLE `usuarios`
   ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`Idrol`) REFERENCES `roles` (`Idrol`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
