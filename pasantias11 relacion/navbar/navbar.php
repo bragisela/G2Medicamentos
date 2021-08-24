1dbc36b4c618fa59e07c3c285b54e6bb15e859cd
ThomyFlores <you@example.com>
2021-08-23 21:11:50 -0300

    a

---
+++ pasantias11 relacion/navbar/navbar.php
@@ -0,0 +1,32 @@
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Inicio</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="clearing.php">Clearing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="clsbotiquin.php">clsbotiquin</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="salidas.php">salidas</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="recetas.php">Recetas</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="stockinicial.php">stockinicial</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
