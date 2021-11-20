76b4f7bddd62123f51120d5bf9bde71ea73dfcbc
ThomyFlores <you@example.com>
2021-11-20 17:43:03 -0300

    a

--- pasantias/Datatable/javascripts/js.js
+++
@@ -1,8 +0,0 @@
-function Caps()
-  {
-    var prov=document.getElementById('usuarios');
-  	document.getElementById('eleccion').value=prov.options[prov.selectedIndex].value;
-
-	var prov=document.getElementById('usuarios');
-	document.getElementById('name').value=prov.options[prov.selectedIndex].text;
-  }
